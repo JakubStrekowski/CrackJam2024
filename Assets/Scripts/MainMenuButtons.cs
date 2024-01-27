64df699dbc08d795500668e64966183455776f15
jakub.strekowski <jakub.strekowski@plum.pl>
2024-01-27 04:53:25 +0100

    Add explicit content warning

--- Assets/Scripts/MainMenuButtons.cs
+++ Assets/Scripts/MainMenuButtons.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuButtons : MonoBehaviour
 {
+    [SerializeField] private Button adultConsent;
+    
     private const int GameplaySceneId = 1;
 
     public AudioMixer mixer;
@@ -27,6 +30,11 @@ public class MainMenuButtons : MonoBehaviour
     public void SetLevel (float sliderValue)
     {
         mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetConsent(bool value)
+    {
+        adultConsent.interactable = value;
     }
     
 }
